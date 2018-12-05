35c945abbe529a65dc60e59dcdd7853f56a92cab
Federico Terzi <freddy6896@gmail.com>
2018-12-05 18:34:43 +0100

    First experiment with virtual classes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <memory>
+#include "lexer/Lexer.h"
+#include "lexer/token/BaseToken.h"
+#include "lexer/token/TextToken.h"
+
+using namespace ninx::lexer;
+using namespace ninx::lexer::token;
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Lexer lexer;
+
+    unique_ptr<token::BaseToken> token { make_unique<token::TextToken>("Bella raga") };
+
+    auto t { dynamic_cast<TextToken*>(token.get()) };
+
+    cout << t->get_text();
+
     return 0;
 }
